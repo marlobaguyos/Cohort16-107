cc2fb07a5ae97c602eebc1cae87cef014265fede
Marlo <marlo.baguyos@outlook.com>
2021-03-24 12:28:27 -0400

    update

--- intro/Program.cs
+++ intro/Program.cs
@@ -7,6 +7,55 @@ namespace intro
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+        }
+        //access_level return_type method_name (params)
+        public int Variable()
+        {
+            string name = "Sergio";
+            int age = 34;
+            float distance = 3445.423f;
+            decimal price = 29.99m;
+            bool found = false;
+
+            //conditional blocks
+            if(age < 100)
+            {
+                Console.WriteLine("Don't worry, you are still young");
+            }
+
+            return 1;
         }
     }
 }
+
+// JS = pascalCase
+// CS = CamelCase
+
+//Interpreters
+
+/*
+js,python,php,Ror,
+
+Code 
+    Run
+        Read
+        Syntax
+        Parse
+        Exec
+
+*/
+
+//Compliled
+/*
+    C#, Java,C,C++
+
+    Code
+    Compile
+        Read
+        Syntax
+        Parse
+        Save the compilation results into binary format
+    Run
+        execute
+*/
