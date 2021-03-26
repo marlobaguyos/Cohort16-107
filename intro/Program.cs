222268cffb3789fcddcdd0f2f0f3099897b140ff
Marlo <marlo.baguyos@outlook.com>
2021-03-26 00:10:08 -0400

    update

--- intro/Program.cs
+++ intro/Program.cs
@@ -12,8 +12,49 @@ namespace intro
             p.Variables();
             p.TestCollections();
 
+            int result = p.SumOfNumbers(21,21);
+            System.Console.WriteLine(result);  //The answer to life the universe and evertying :D
+
+            int greatest = p.GreatestNumber(123,1987612, 98723);
+            // System.Console.WriteLine(greatest); //The answer to life the universe and evertying :
         }
+
+        /*
+            Access levels:
+                public
+                private
+                protected
+                default (empty)
+        */
+
+
+        int GreatestNumber(int n1, int n2, int n3)
+        {
+            if(n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("{0} is the greatest number of the three numbers | {0}, {1}, {2}", n1, n2, n3);
+                return n1;
+            }
+            else if(n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("{1} is the greatest number of the three numbers | {0}, {1}, {2}", n1, n2, n3);
+                return n2;
+            }
+            else
+            {
+                Console.WriteLine("{2} is the greatest number of the three numbers | {0}, {1}, {2}", n1, n2, n3);
+                return n3;
+            }
+
+        }
+        int SumOfNumbers(int num1, int num2)
+        {
+            int res = num1 + num2;
+            return res;
+        }
+
         //access_level return_type method_name (params)
+        
         public int Variables()
         {
             string name = "Sergio";
@@ -51,7 +92,7 @@ namespace intro
         public void TestCollections()
         {
             //arrays
-            string[] names = new string[3000000000];
+            string[] names = new string[1000];
             names[0] = "Nick";
             names[1] = "Jay";
             names[3] = "Eric";
@@ -63,13 +104,18 @@ namespace intro
             colors.Add("Dark Yellow");
             colors.Add("blue");
             colors.Add("puRpLE");
-            colors.Add("puRpdfafsdfasdfasdLE");
+            colors.Add("black");
 
             System.Console.WriteLine(colors);
 
             for(int i=0; i<colors.Count; i++)
             {
                 string color = colors[i];
+                System.Console.WriteLine(color);
+            }
+
+            foreach(string color in colors)
+            {
                 System.Console.WriteLine(color);
             }
         }
